5ad6d061d84f8b94ae5e2e91099ad6a2cf9abb4d
Sourav Kumar Sahu <41680507+SouravKumarSahu@users.noreply.github.com>
2020-05-05 22:52:25 +0530

    Update MACHINE_PLATFORM_DETAILS.py

--- MACHINE_PLATFORM_DETAILS.py
+++ MACHINE_PLATFORM_DETAILS.py
@@ -1,9 +1,12 @@
-from platform import platform, machine, processor, system, version, python_implementation, python_version_tuple
+from platform import platform, machine, processor, system, version, python_implementation, python_version_tuple, architecture, uname
 
 print("OS DETAILS : ", platform())                              #Windows-10-10.0.19613-SP0
-print("HARDWARE : ", machine())                                 #AMD64
+print("SIZE OF REGISTER AVAILABLE : ", machine())               #AMD64
 print("PROCESSOR : ", processor())                              #Intel64 Family 6 Model 78 Stepping 3, GenuineIntel
 print("OS NAME : ", system())                                   #Windows
 print("OS VERSION : ", version())                               #10.0.19613
 print("Python COMPILER : ", python_implementation())            #CPython
 print("Python COMPILER VERSION : ", python_version_tuple())     #('3', '6', '5')
+print("OS ARCHITECTURE : ", architecture())                     #('32bit', 'WindowsPE')
+
+print("ALL DETAILS : ", uname())                                #uname_result(system='Windows', node='DESKTOP-BOKSTOB', release='10', version='10.0.19613', machine='AMD64', processor='Intel64 Family 6 Model 78 Stepping 3, GenuineIntel')
