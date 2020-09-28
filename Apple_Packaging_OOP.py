7e01b9023909dc9876af6e8ffa2f59a92b8e23b5
Sourav Kumar Sahu <41680507+SouravKumarSahu@users.noreply.github.com>
2020-09-28 21:48:24 +0530

    Create Apple_Packaging_OOP.py

---
+++ Apple_Packaging_OOP.py
@@ -0,0 +1,44 @@
+import random
+
+class Apple:
+    def __init__(self):
+        self.weight = random.uniform(0.2, 0.5)
+        
+class Package:
+    total_no_of_apples = 0
+    max_weight_per_pack = 50
+    
+    def __init__(self,pack_num):
+        self.pack_num = pack_num
+        self.apples_in_this_pack = 0
+        self.weight_of_this_pack = 0
+        
+    def add_apple(self,apple):
+        self.weight_of_this_pack += apple.weight
+        if self.weight_of_this_pack < Package.max_weight_per_pack:
+            self.apples_in_this_pack += 1
+            Package.total_no_of_apples += 1
+            return False
+        else:
+            #print(f"package {self.pack_num} is full, use another !!!")
+            return True
+            
+package_full = True
+
+conatiner = []
+
+for i in range(1,1000):
+    if package_full:
+        new_package = Package(f"package_{i}")
+        conatiner.append(new_package)
+        package_full = False
+    else:
+        package_full = new_package.add_apple(Apple())
+        
+print(f"Number of packages {len(conatiner)} with total apples {Package.total_no_of_apples}")
+
+for x in conatiner:
+    print(f"Package: {x.pack_num} contains {x.apples_in_this_pack} apples and weigh around {x.weight_of_this_pack}")
+        
+        
+        
